e1842a7ac61fa9b908e4546af5ec6f58e16bc411
Akbarali Otakhanov <otakhanov.akbarali@gmail.com>
2024-05-20 18:37:52 +0900

    user-log commits

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,14 @@ Route::get('/pizzas/create', [PizzaController::class,'create']);
 Route::post('/pizzas', [PizzaController::class, 'store']);
 Route::get('/pizzas/{id}', [PizzaController::class, 'show']);
 Route::delete('/pizzas/{id}', [PizzaController::class, 'destroy']);
+
+
+
+//user_log.php
+//<?php
+//Route::post('/api/user_log', 'API\Info\UserLogController@user_log');
+//Route::post('/api/upload/user_log', 'API\Upload\UserLogUploadController@upload');
+//Route::get('/api/download/user_log', 'API\Download\UserLogDownloadController@download');
+
+//Route::any('/api/user_log/{APIName}','API\Info\UserLogController@Route');
+//?>
